a3b988b2b603f42d9f504ede349ff79543624a0c
Nurul-GC <nuruldecarvalho@gmail.com>
2021-10-10 04:54:55 +0200

    Update app

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,10 @@ here goes all
 definition of our app instance
 """
 
-from flask import Flask, Blueprint
+from flask import Flask
 from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-routes = Blueprint('routes', __name__)
 
 from app import routers
