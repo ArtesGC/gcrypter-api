a8096407200c58e4461258a6b217d884935706f2
Nurul-GC <nuruldecarvalho@gmail.com>
2021-10-10 03:50:44 +0200

    Update app/config

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -3,10 +3,10 @@ here we'll
 define our virtual-variables
 as configuration to our project
 """
+from secrets import token_hex
 
 
 class Config:
     """project's configuration"""
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database/gc.db'
+    SECRET_KEY = token_hex(32)
+    DEBUG = False
