c268cb4e2153b30a914c32c1fecc9dd5446a7e94
Nurul-GC <nuruldecarvalho@gmail.com>
2021-10-10 04:55:34 +0200

    Update app/routers

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,15 +1,15 @@
 """defining our routes"""
 
-from app import routes, gcrypter
+from app import app, gcrypter
 from flask import render_template, request, jsonify, redirect, url_for
 
 
-@routes.route("/", methods=['GET'])
+@app.route("/", methods=['GET'])
 def index():
     return render_template("index.html")
 
 
-@routes.route("/encrypt/", methods=['GET', 'POST'])
+@app.route("/encrypt/", methods=['GET', 'POST'])
 def encrypt():
     if request.query_string:
         text = request.query_string.decode().split('=')[1]
@@ -17,10 +17,10 @@ def encrypt():
     elif request.method == 'POST':
         text = request.form.to_dict()['text']
         return jsonify(encrypted=gcrypter.encrypt(t=text))
-    return redirect(url_for('routes.index'))
+    return redirect(url_for('index'))
 
 
-@routes.route("/decrypt/", methods=['GET', 'POST'])
+@app.route("/decrypt/", methods=['GET', 'POST'])
 def decrypt():
     if request.query_string:
         values = request.query_string.decode().split('&')
@@ -32,4 +32,4 @@ def decrypt():
         value1 = int(values['value1'])
         value2 = int(values['value2'])
         return jsonify(decrypted=gcrypter.decrypt(v1=value1, v2=value2))
-    return redirect(url_for('routes.index'))
+    return redirect(url_for('index'))
