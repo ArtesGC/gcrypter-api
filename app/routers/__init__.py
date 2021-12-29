e8036cfd3808333c9836db8b43810df3486fa505
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-29 19:36:11 +0200

    Update app/routers

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,7 +1,8 @@
 """defining our routes"""
 
-from app import app, gcrypter
+from app import app
 from flask import render_template, request, jsonify, redirect, url_for
+from gcrypter import encrypt, decrypt
 
 
 @app.route("/", methods=['GET'])
@@ -9,27 +10,33 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/encrypt/", methods=['GET', 'POST'])
-def encrypt():
+@app.route("/encryption/", methods=['GET', 'POST'])
+def encryption():
     if request.query_string:
         text = request.query_string.decode().split('=')[1]
-        return jsonify(encrypted=gcrypter.encrypt(t=text))
+        return jsonify(encrypted=encrypt(text=text))
     elif request.method == 'POST':
         text = request.form.to_dict()['text']
-        return jsonify(encrypted=gcrypter.encrypt(t=text))
+        return jsonify(encrypted=encrypt(text=text))
     return redirect(url_for('index'))
 
 
-@app.route("/decrypt/", methods=['GET', 'POST'])
-def decrypt():
+@app.route("/decryption/", methods=['GET', 'POST'])
+def decryption():
     if request.query_string:
-        values = request.query_string.decode().split('&')
-        value1 = int(values[0].split('=')[1])
-        value2 = int(values[1].split('=')[1])
-        return jsonify(decrypted=gcrypter.decrypt(v1=value1, v2=value2))
+        if '&' in request.query_string.decode():
+            values = request.query_string.decode().split('&')
+            value1 = int(values[0].split('=')[1])
+            value2 = int(values[1].split('=')[1])
+            return jsonify(decrypted=decrypt(text_enc=(value1, value2)))
+        else:
+            values = request.query_string.decode().split('=')[1].split(',')
+            value1 = int(values[0][1:])
+            value2 = int(values[1][:-1])
+            return jsonify(decrypted=decrypt(text_enc=(value1, value2)))
     elif request.method == 'POST':
         values = request.form.to_dict()
         value1 = int(values['value1'])
         value2 = int(values['value2'])
-        return jsonify(decrypted=gcrypter.decrypt(v1=value1, v2=value2))
+        return jsonify(decrypted=decrypt(text_enc=(value1, value2)))
     return redirect(url_for('index'))
